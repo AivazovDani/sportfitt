cdcdd3e7820b42ed3db44d9af37edf18f26cb0e1
AivazovDani <ayvazov.dani@gmail.com>
2025-08-06 08:25:11 -0700

    Add placeholder DashboardTabs component

---
+++ components/dashboard-tabs.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+export function DashboardTabs() {
+  return (
+    <div>
+      {/* Placeholder component for dashboard tabs */}
+    </div>
+  );
+}
+
+export default DashboardTabs;
